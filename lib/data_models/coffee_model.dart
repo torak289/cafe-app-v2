bfe31c0e7739645fea51554aa0ee92ecdd8cb142
Wills-Mac <torak289@gmail.com>
2024-10-16 11:55:33 +0100

    added description to coffee_model for future use

--- lib/data_models/coffee_model.dart
+++ lib/data_models/coffee_model.dart
@@ -1,9 +1,13 @@
 class CoffeeModel {
   String name;
+  String? description;
 
-  CoffeeModel({required this.name});
-
+  CoffeeModel({required this.name, this.description});
+  
   factory CoffeeModel.fromJson(Map<String, dynamic> data) {
-    return CoffeeModel(name: data['name']);
+    return CoffeeModel(
+      name: data['name'],
+      description: data['description'],
+    );
   }
 }
