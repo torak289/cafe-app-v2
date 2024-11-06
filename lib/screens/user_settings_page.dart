c75c30e05b97a74b9e0309dbcfdffa03f83d787c
Wills-Mac <torak289@gmail.com>
2024-11-06 22:46:51 +0000

    Added Confirm Popup Flutter & Navigator pop behaviour

--- lib/screens/user_settings_page.dart
+++ lib/screens/user_settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:cafeapp_v2/constants/Cafe_App_UI.dart';
+import 'package:cafeapp_v2/constants/routes.dart';
 import 'package:cafeapp_v2/data_models/user_model.dart';
 import 'package:cafeapp_v2/services/auth_service.dart';
 import 'package:flutter/material.dart';
@@ -18,7 +19,8 @@ class UserSettingsPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded( //TODO: Impement SingleChildScrollView
+            Expanded(
+              //TODO: Impement SingleChildScrollView
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -43,9 +45,51 @@ class UserSettingsPage extends StatelessWidget {
                   style: const ButtonStyle(
                     backgroundColor: WidgetStatePropertyAll(Colors.red),
                   ),
-                  onPressed: () {
-                    //TODO: Implement Delete Account Process...
-                  },
+                  onPressed: () => showDialog<String>(
+                    //TODO: Implement Async Stream to listen to database events fired on Scan from Cafe Account...
+                    //TODO: Implement this for both available QR Codes...
+                    //TODO: Pop context and update UI state on Scan...
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                      content: const Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            "Are you sure you want to delete your account?",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsets.all(CafeAppUI.buttonSpacingSmall),
+                          ),
+                          Text(
+                              'This is a permanent action and cannot be undone. Make sure you want to do this!'),
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () async {
+                            //TODO: Implement authService account deletion...
+                            
+                            Navigator.popUntil(context, (route) {
+                              return route.settings.name == Routes.mapPage;
+                            });
+                          },
+                          style: const ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(Colors.red),
+                          ),
+                          child: const Text("Yes"),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, "Closed"),
+                          child: const Text("No"),
+                        ),
+                      ],
+                    ),
+                  ),
                   child: const Text('Delete Account'),
                 ),
                 const SizedBox(
