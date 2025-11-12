e4791d7fb9b89769f38dd4069b5db5f6f3d0aa2b
Wills-Mac <torak289@gmail.com>
2025-11-12 10:58:47 +0000

    added fields for instagram account

--- lib/screens/add_cafe_page.dart
+++ lib/screens/add_cafe_page.dart
@@ -162,6 +162,15 @@ class _AddCafePageState extends State<AddCafePage>
                   controller: cafeName,
                 ),
                 const Padding(padding: EdgeInsets.all(CafeAppUI.buttonSpacingMedium)),
+                /*const Text(
+                  'Instagram',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const Padding(padding: EdgeInsets.all(CafeAppUI.buttonSpacingSmall)),
+                TextField(
+                  controller: cafeName,
+                ),*/
+                const Padding(padding: EdgeInsets.all(CafeAppUI.buttonSpacingMedium)),
                 const Text(
                   'Description',
                   style: TextStyle(fontWeight: FontWeight.bold),
