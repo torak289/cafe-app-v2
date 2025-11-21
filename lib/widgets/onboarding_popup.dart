1657bed31ee9d42a1addef9cba18bf4610d5873c
Wills-Mac <torak289@gmail.com>
2025-11-21 10:24:08 +0000

    added onboarding popup as widget

---
+++ lib/widgets/onboarding_popup.dart
@@ -0,0 +1,83 @@
+import 'package:cafeapp_v2/constants/Cafe_App_UI.dart';
+import 'package:cafeapp_v2/services/share_pref_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class OnboardingPopup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final SharePrefService sharePrefService =
+        Provider.of<SharePrefService>(context, listen: false);
+    final pageController = PageController(viewportFraction: 1, keepPage: true);
+    if (sharePrefService.isFirstLauncher) {
+      return Positioned.fill(
+        child: GestureDetector(
+          onTap: () {
+            sharePrefService.hasFirstLaunched();
+          },
+          child: Container(
+            color: Colors.black54, // Dim background
+            child: Center(
+              child: AlertDialog(
+                backgroundColor: CafeAppUI.backgroundColor,
+                content: SizedBox(
+                  height: 320,
+                  width: 200,
+                  child: PageView(
+                    controller: pageController,
+                    scrollDirection: Axis.horizontal,
+                    pageSnapping: true,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/Cafe_Logo.png',
+                            scale: 2,
+                          ),
+                          Padding(
+                              padding: EdgeInsetsGeometry.all(
+                                  CafeAppUI.buttonSpacingMedium)),
+                          Text(
+                              'Welcome to Robusta the community-powered coffee app built for independent coffee lovers.'),
+                          Padding(
+                            padding: EdgeInsetsGeometry.all(
+                                CafeAppUI.buttonSpacingSmall),
+                          ),
+                        ],
+                      ),
+                      Center(
+                        child: Text('Onboarding Page 2'),
+                      ),
+                      Center(
+                        child: Text('Onboarding Page 3'),
+                      ),
+                      Center(
+                        child: Text('Onboarding Page 4'),
+                      ),
+                    ],
+                  ),
+                ),
+                actionsAlignment: MainAxisAlignment.center,
+                actions: [
+                  Column(
+                    children: [
+                      TextButton(
+                          onPressed: () {
+                            sharePrefService.hasFirstLaunched();
+                          },
+                          child: Text('Skip Onboarding')),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    } else {
+      return SizedBox.shrink();
+    }
+  }
+}
