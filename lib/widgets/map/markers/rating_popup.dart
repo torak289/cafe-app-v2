8f5798f87810942c552f6106af067f33b5cb21bf
Wills-Mac <torak289@gmail.com>
2025-11-23 11:14:19 +0000

    Error checking for 0 star ratings

--- lib/widgets/map/markers/rating_popup.dart
+++ lib/widgets/map/markers/rating_popup.dart
@@ -14,6 +14,7 @@ class rating_popup extends StatefulWidget {
   double coffeeRating = 0;
   double atmosphereRating = 0;
   double serviceRating = 0;
+  bool showError = false;
 
   @override
   State<rating_popup> createState() => _rating_popupState();
@@ -24,6 +25,9 @@ class _rating_popupState extends State<rating_popup> {
   Widget build(BuildContext context) {
     DatabaseService databaseService =
         Provider.of<DatabaseService>(context, listen: false);
+
+    final formKey = GlobalKey<FormState>();
+
     return DraggableScrollableSheet(
         initialChildSize: 1,
         expand: false,
@@ -38,157 +42,196 @@ class _rating_popupState extends State<rating_popup> {
                   48,
                   48,
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Center(
-                      child: Text(
-                        'Rate ${widget.cafe.name}',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsetsGeometry.all(CafeAppUI.buttonSpacingMedium),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Coffee: ',
-                        ),
-                        StarRating(
-                          size: 32,
-                          rating: widget.coffeeRating,
-                          allowHalfRating: false,
-                          color: CafeAppUI.iconButtonIconColor,
-                          borderColor: CafeAppUI.iconButtonIconColor,
-                          onRatingChanged: (rating) => setState(() {
-                            widget.coffeeRating = rating;
-                          }),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsetsGeometry.all(CafeAppUI.buttonSpacingSmall),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Service: ',
-                        ),
-                        StarRating(
-                          size: 32,
-                          rating: widget.serviceRating,
-                          allowHalfRating: false,
-                          color: CafeAppUI.iconButtonIconColor,
-                          borderColor: CafeAppUI.iconButtonIconColor,
-                          onRatingChanged: (rating) => setState(() {
-                            widget.serviceRating = rating;
-                          }),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsetsGeometry.all(CafeAppUI.buttonSpacingSmall),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Atmosphere: ',
-                        ),
-                        StarRating(
-                          size: 32,
-                          rating: widget.atmosphereRating,
-                          allowHalfRating: false,
-                          color: CafeAppUI.iconButtonIconColor,
-                          borderColor: CafeAppUI.iconButtonIconColor,
-                          onRatingChanged: (rating) => setState(
-                            () {
-                              widget.atmosphereRating = rating;
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsetsGeometry.all(CafeAppUI.buttonSpacingMedium),
-                    ),
-                    Row(
-                      children: [
-                        const Text(
-                          'Review ',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        const Text(
-                          ' Optional',
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Rate ${widget.cafe.name}',
                           style: TextStyle(
-                            color: Colors.pinkAccent,
+                            fontSize: 24,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                      ],
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsetsGeometry.all(CafeAppUI.buttonSpacingSmall),
-                    ),
-                    TextFormField(
-                      minLines: 10,
-                      maxLines: 10,
-                      maxLength: 256,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(16),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
-                        ),
-                        errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
-                        ),
-                        focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.all(
+                            CafeAppUI.buttonSpacingMedium),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Coffee: ',
+                          ),
+                          StarRating(
+                            size: 32,
+                            rating: widget.coffeeRating,
+                            allowHalfRating: false,
+                            color: CafeAppUI.iconButtonIconColor,
+                            borderColor: CafeAppUI.iconButtonIconColor,
+                            onRatingChanged: (rating) => setState(() {
+                              widget.coffeeRating = rating;
+                            }),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.all(
+                            CafeAppUI.buttonSpacingSmall),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Service: ',
+                          ),
+                          StarRating(
+                            size: 32,
+                            rating: widget.serviceRating,
+                            allowHalfRating: false,
+                            color: CafeAppUI.iconButtonIconColor,
+                            borderColor: CafeAppUI.iconButtonIconColor,
+                            onRatingChanged: (rating) => setState(() {
+                              widget.serviceRating = rating;
+                            }),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.all(
+                            CafeAppUI.buttonSpacingSmall),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Atmosphere: ',
+                          ),
+                          StarRating(
+                            size: 32,
+                            rating: widget.atmosphereRating,
+                            allowHalfRating: false,
+                            color: CafeAppUI.iconButtonIconColor,
+                            borderColor: CafeAppUI.iconButtonIconColor,
+                            onRatingChanged: (rating) => setState(
+                              () {
+                                widget.atmosphereRating = rating;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.all(
+                            CafeAppUI.buttonSpacingMedium),
+                      ),
+                      Row(
+                        children: [
+                          const Text(
+                            'Review ',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          const Text(
+                            ' Optional',
+                            style: TextStyle(
+                              color: Colors.pinkAccent,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.all(
+                            CafeAppUI.buttonSpacingSmall),
+                      ),
+                      TextFormField(
+                        minLines: 10,
+                        maxLines: 10,
+                        maxLength: 256,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(16),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsetsGeometry.all(CafeAppUI.buttonSpacingLarge),
-                    ),
-                    TextButton(
-                      onPressed: () async {
-                        try {
-                          bool success = await databaseService.reviewCafe(
-                            widget.cafe.uid!,
-                            widget.coffeeRating,
-                            widget.atmosphereRating,
-                            widget.serviceRating,
+                      Builder(builder: (context) {
+                        if (widget.showError) {
+                          return Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsetsGeometry.all(
+                                    CafeAppUI.buttonSpacingMedium),
+                              ),
+                              Text(
+                                "You must review the Cafe! We don't submit 0 Stars",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: CafeAppUI.errorText,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsetsGeometry.all(
+                                    CafeAppUI.buttonSpacingLarge),
+                              ),
+                            ],
                           );
-                          debugPrint('On Pressed Review: $success');
-                          if (context.mounted) {
-                            Navigator.pop(context);
-                          }
-                        } catch (e) {
-                          debugPrint(e.toString());
+                        } else {
+                          return Padding(
+                            padding: EdgeInsetsGeometry.all(
+                                CafeAppUI.buttonSpacingLarge),
+                          );
                         }
-                      },
-                      child: Text('Review'),
-                    ),
-                  ],
+                      }),
+                      TextButton(
+                        onPressed: () async {
+                          if (formKey.currentState!.validate()) {
+                            if (widget.atmosphereRating < 1 ||
+                                widget.coffeeRating < 1 ||
+                                widget.serviceRating < 1) {
+                              setState(() {
+                                widget.showError = true;
+                                debugPrint(
+                                    "You must review the cafe! We don't submit 0 Stars");
+                              });
+                              return;
+                            }
+                            try {
+                              bool success = await databaseService.reviewCafe(
+                                widget.cafe.uid!,
+                                widget.coffeeRating,
+                                widget.atmosphereRating,
+                                widget.serviceRating,
+                              );
+                              debugPrint('On Pressed Review: $success');
+                              if (context.mounted) {
+                                Navigator.pop(context);
+                              }
+                            } catch (e) {
+                              debugPrint(e.toString());
+                            }
+                          }
+                        },
+                        child: Text('Review'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
