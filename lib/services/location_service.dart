b0152b79a091ec23a083973fb93ca22de932f6dd
Wills-Mac <torak289@gmail.com>
2025-12-01 18:12:21 +0000

    added kalman filter for latlng stability

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -22,11 +22,60 @@ class LocationService extends ChangeNotifier {
     }
     return permission;
   }
+
   Stream<Position> positionStream = Geolocator.getPositionStream();
 
   Future<Position> currentPosition = Geolocator.getCurrentPosition();
 
-  void openLocationSetting(){
+  void openLocationSetting() {
     Geolocator.openAppSettings();
+  }
+}
+
+class LocationFilter {
+  double _errorEstimate = 1.0;
+  double _lastEstimate = 0.0;
+  double _kalmanGain = 0.0;
+  final double _errorMeasure = 0.1;
+  final double _errorProcess = 0.01;
+
+  List<double> filterCoordinates(List<double> rawValues) {
+    List<double> filteredValues = [];
+
+    for (double currentMeasurement in rawValues) {
+      // Prediction step
+      double prediction = _lastEstimate;
+      _errorEstimate = _errorEstimate + _errorProcess;
+
+      // Update step
+      _kalmanGain = _errorEstimate / (_errorEstimate + _errorMeasure);
+      double currentEstimate =
+          prediction + _kalmanGain * (currentMeasurement - prediction);
+      _errorEstimate = (1.0 - _kalmanGain) * _errorEstimate;
+
+      // Save for next iteration
+      _lastEstimate = currentEstimate;
+      filteredValues.add(currentEstimate);
+    }
+
+    return filteredValues;
+  }
+
+  Position filterLocation(Position location) {
+    final filteredLat = filterCoordinates([location.latitude])[0];
+    final filteredLng = filterCoordinates([location.longitude])[0];
+
+    return Position(
+      longitude: filteredLat,
+      latitude: filteredLng,
+      timestamp: location.timestamp,
+      accuracy: location.accuracy,
+      altitude: location.altitude,
+      altitudeAccuracy: location.altitudeAccuracy,
+      heading: location.heading,
+      headingAccuracy: location.headingAccuracy,
+      speed: location.speed,
+      speedAccuracy: location.speedAccuracy,
+    );
   }
 }
