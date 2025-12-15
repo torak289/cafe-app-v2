b66d63ff5b9c051048202217182f5ca9a8fc7d09
Wills-Mac <torak289@gmail.com>
2025-12-15 13:14:16 +0000

    added better map caching - vibe coded needs review

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:cafeapp_v2/services/map_cache_service.dart';
 import 'cafe_app.dart';
 
 void main() async {
@@ -9,5 +10,7 @@ void main() async {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyanpseG1zYXNvYmx5aXJkbmVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4NjAwMzIsImV4cCI6MjAzODQzNjAzMn0.pxI68RjlxtpEDzpU_vLQs9JM8W1LxR1K7A3fw3nLeEY',
   );
   await Hive.initFlutter();
+  // Initialize map tile cache service early to ensure cache directory is ready
+  await MapCacheService.initialize();
   runApp(CafeApp());
 }
